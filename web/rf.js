bb664ec4a7fa893ffdcafb7374071a8ebf3ab0d1
jojonki <junki.ohmura@gmail.com>
2021-03-13 23:19:21 +0900

    dynamic custom

--- web/rf.js
+++ web/rf.js
@@ -31,11 +31,12 @@ class Variable {
 }
 
 class ConvLayer {
-    constructor(name, kernel_size, stride, padding) {
+    constructor(name, kernel_size, stride, padding, dilation) {
         this.name = name;
         this.kernel_size = kernel_size;
         this.stride = stride;
         this.padding = padding;
+        this.dilation = dilation;
     }
 
     forward(x) {
@@ -69,95 +70,115 @@ class ConvLayer {
 
 
 function init() {
-    var stage = new createjs.Stage("demoCanvas");
+    var stage = new createjs.Stage("rf-canvas");
+    MyNet = [];
+    let n_layers = localStorage.getItem('n_layers');
+    console.log(n_layers);
+    if (n_layers == null) {
+        stage.removeAllChildren();
+        stage.update();
+        return;
+    }
+
+    for (let i = 1; i <= n_layers; i++) {
+        let layer_name = 'layer' + i;
+        let layer_info = JSON.parse(localStorage.getItem(layer_name));
+        console.log(layer_info);
+        MyNet.push(
+            new ConvLayer(layer_name, layer_info.kernel, layer_info.stride, layer_info.padding, layer_info.dilation)
+        )
+        console.log(layer_name, layer_info.kernel, layer_info.stride, layer_info.padding, layer_info.dilation);
+    }
+
+
+
     let in_color = "#4285F4";
     let line_color = "Black";
     // let layer_colors = ["#FBBC05", "#34A853", "#EA4335"];
     // https://coolors.co/palettes/trending
     let layer_colors = ["#ef476f", "#ffd166", "#06d6a0", "#118ab2", "#073b4c"];
-    let w = 30;
+    let w = 14;
+    let h = w;
     let hw = w / 2;
-    let L = 40;
-    let offset = 4;
+    // let L = 80;
+    let L = parseInt(document.getElementById("textInputSize").value);
+    let offset = 2;
     let stride_width = w + offset;
-    let stride_height = 120;
-    let most_left_x = 20;
+    let stride_height = 80;
+    let most_left_x = 150;
 
     let data = new Variable(n_in = L, j_in = 1, r_in = 1, start_in = 0.5);
+    let y = stride_height;
     // draw input data
     for (let i = 0; i < data.n_in; i++) {
         // var circle = new createjs.Shape();
         let x = most_left_x + stride_width * i;
-        let y = stride_height;
         // circle.graphics.beginFill(in_color).drawRect(x, y, w, w);
-        drawRect(stage, x, y, w, w, in_color);
+        drawRect(stage, x, y, w, h, in_color);
         // stage.addChild(circle);
     }
+    // text
+    var text = new createjs.Text('Input\nL=' + L, "20px serif", in_color);
+    text.y += y;
+    stage.addChild(text);
 
-    MyNet = [
-        new ConvLayer('conv1', 3, 3, 1),
-        new ConvLayer('conv2', 3, 3, 1),
-        // new ConvLayer('conv3', 3, 2, 1),
-        // new ConvLayer('conv3', 3, 2, 1),
-    ];
+
+
     let rf_list = []
 
-    let n_layers = MyNet.length;
     let rep_origin_x = null;
     let left_offset = 0;
-    for (let layer = 0; layer < n_layers; layer++) {
+    for (let layer = 0; layer < MyNet.length; layer++) {
         net = MyNet[layer];
-        data = net.forward(data);
-        rf_list.push(data.r_in);
         let kernel = net.kernel_size;
         let stride = net.stride;
-
-        // debug info
-        var layer_info = 'Layer ' + (layer + 1) + ", K=" + kernel + ", S=" + stride + ", RF=" + rf_list[rf_list.length - 1];
-        var text = new createjs.Text(layer_info, "24px serif", "Red");
-        text.y += 30 * layer
-        stage.addChild(text);
+        let color = layer_colors[layer % layer_colors.length];
+        let y = stride_height * (layer + 2)
+        L = Math.floor((L - kernel) / stride) + 1;
 
         // draw layer
-        let color = layer_colors[layer % layer_colors.length];
-        let y = 80 * layer;
-        L = Math.floor((L - kernel) / stride) + 1;
-        // console.log(L);
+        console.log('L=' + L);
+        if (L <= 0) continue;
+        data = net.forward(data);
+        rf_list.push(data.r_in);
+
+        // text
+        var layer_info = 'Layer ' + (layer + 1) + "\nK=" + kernel + ", S=" + stride + "\nRF=" + rf_list[rf_list.length - 1]
+            + '\nL=' + L;
+        var text = new createjs.Text(layer_info, "18px serif", color);
+        text.y += y - 30;
+        stage.addChild(text);
+
         let prev_stride_width = stride_width;
         stride_width = stride * stride_width;
         left_offset = (data.r_in - 1) * (w + offset) / 2;
         for (let i = 0; i < L; i++) {
             // console.log(left_offset);
             let x = most_left_x + left_offset + stride_width * i;
-            let y = stride_height * (layer + 2)
             // console.log(layer, i, x, y);
             let is_focus_ndoe = (layer == n_layers - 1 && i == Math.floor((L - 1) / 2)) ? true : false;
             if (is_focus_ndoe) { // base data for RF
                 rep_origin_x = x;
-                drawRect(stage, x, y, w, w, 'Green');
+                drawRect(stage, x, y, w, h, 'Green');
             } else {
-                drawRect(stage, x, y, w, w, color);
+                drawRect(stage, x, y, w, h, color);
             }
 
+            // draw lines
             let prev_y = y - stride_height + w;
             for (let j = 0; j < kernel; j++) {
-                // let rep_width = rep_fields[layer] * (w + offset);
-                // let from_x = x - (w + offset) * (rep_fields[layer] - 1) / 2 - offset / 2 + hw + offset;
-                let from_x = x - prev_stride_width + offset / 2 + hw;//+ hw + offset;
+                let from_x = (x + hw) - prev_stride_width * (kernel - 1) / 2;
                 if (is_focus_ndoe) { // base data for RF
                     drawLine(stage, x + hw, y, from_x + (prev_stride_width) * j, prev_y, 'Green');
                 } else {
-                    // drawLine(stage, x + hw, y, x - prev_stride_w + prev_stride_w * j + hw, prev_y, 'Green');
-                    // drawLine(stage, x + hw, y, rep_x + (w + offset) * j, prev_y, line_color);
                     drawLine(stage, x + hw, y, from_x + (prev_stride_width) * j, prev_y, line_color);
-
                 }
             }
         }
     }
 
     // draw receptive field
-    for (let layer = 0; layer < n_layers; layer++) {
+    for (let layer = 0; layer < rf_list.length; layer++) {
         let rep_field = rf_list[layer];
         let rep_width = rep_field * (w + offset);
         // let rep_x = rep_origin_x - (rep_field - 1) * (w + offset) / 2 - offset / 2;
@@ -165,9 +186,8 @@ function init() {
         let rep_x = rep_origin_x - (w + offset) * (rf_list[layer] - 1) / 2 - offset / 2;
         let rep_y = stride_height - (hw * (layer + 1)) / 2;
         let color = layer_colors[layer % layer_colors.length];
-        drawRect(stage, rep_x, rep_y, rep_width, w + hw * (layer + 1), color, alpha = 0.8, behind = true);
+        drawRect(stage, rep_x, rep_y, rep_width, w + hw * (layer + 1), color, alpha = 0.7, behind = true);
     }
-
 
     stage.update();
 }
